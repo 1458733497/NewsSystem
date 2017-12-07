b5cc7464b63c3dacc891d31007eae904c9323d94
changming <271320256@qq.com>
2017-12-07 15:19:11 +0800

    clear directory

---
+++ gym/Application/Common/Conf/config.php
@@ -0,0 +1,12 @@
+<?php
+// 项目相关配置项
+return array(
+    //URL地址不区分大小写
+    'URL_CASE_INSENSITIVE' =>true,
+    // 加载额外的配置文件
+    'LOAD_EXT_CONFIG' => 'db',
+    // md5后缀,用于用户密码加密
+    'MD5_SUFFIX' => 'JigSaw',
+    // 静态缓存文件后缀
+    'HTML_FILE_SUFFIX' => '.html',
+);
---
+++ gym/Application/Common/Conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    // DB配置项
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'w8258465cl',
+    'DB_PORT' => '3306',
+    'DB_NAME' => 'singcms',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' => 'cms_',
+);
